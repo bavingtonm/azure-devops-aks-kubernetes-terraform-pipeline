3c21b34cd8ee41d90a1e1de270a1f862988a3732
mbavington <bavingtonm@yahoo.co.uk>
2024-10-29 17:35:18 +0000

    linux and Win nodepool added1

--- terraform-manifests/10-aks-cluster-windows-user-nodepools.tf
+++ terraform-manifests/10-aks-cluster-windows-user-nodepools.tf
@@ -1,16 +1,16 @@
 # Create Windows Azure AKS Node Pool
 
-resource "azurerm_kubernetes_cluster_node_pool" "win101" {
+resource "azurerm_kubernetes_cluster_node_pool" "win102" {
   #availability_zones    = [1, 2, 3]
   # Added June 2023
   zones = [ 1, 2, 3 ]
   #enable_auto_scaling  = true # COMMENTED OCT2024
   auto_scaling_enabled = true  # ADDED OCT2024
   kubernetes_cluster_id = azurerm_kubernetes_cluster.aks_cluster.id
-  max_count             = 3
+  max_count             = 1
   min_count             = 1
   mode                  = "User"
-  name                  = "win101"
+  name                  = "win102"
   orchestrator_version  = data.azurerm_kubernetes_service_versions.current.latest_version
   os_disk_size_gb       = 30
   os_type               = "Windows" # Default is Linux, we can change to Windows
